5cae05cd253409d9076da9c1842fd2dc32b431de
Jonny Elliott <jonathan.elliott@cfa.harvard.edu>
2016-01-26 10:25:16 -0500

    init script and virtualenv
    I had to make a view changes to the init script for it to run on bash, hopefully this does not
    break any backwards compatability on your end.
    
    I added the option to install virtualenv, as otherwise you have to re-do the script if you
    do not have it installed (as I did not).

--- init.sh
+++ init.sh
@@ -4,30 +4,46 @@
 # folder with the new name
 
 function search_replace() {
-	folder=${3:-`pwd`}
-	read -p "Recursive '$1' -> '$2' in $folder? [y]:" answer
-  if [ "${answer:-y}" == "y" ]; then
-    echo "find $folder -type f -print0 | xargs -0 sed -i \"s/$1/$2/g\""
-		find $folder -type f -print0 | xargs -0 sed -i "s/$1/$2/g"
-  fi
+    folder=${3:-`pwd`}
+    read -p "Recursive '$1' -> '$2' in $folder? [y]:" answer
+    if [ "${answer:-y}" == "y" ]
+         then
+             echo "find $folder -type f -print0 | xargs -0 sed -i \"s/$1/$2/g\""
+             find $folder -type f -print0 | xargs -0 sed -i "s/$1/$2/g"
+    fi
 }
 
 newname=`basename $1`
 
 read -p "Replace ADSWorker with '$newname'? [y]:" answer
-if [ "${answer:-y}" == "y" ]; then
-	search_replace(ADSWorker $newname ./ADSWorker)
-	search_replace(ADSWorker $newname ./alembic)
-	search_replace(ADSWorker $newname ./manifests)
-	search_replace(ADSWorker $newname './*.ini')
-	search_replace(ADSWorker $newname './*.md')
-	search_replace(ADSWorker $newname './*.txt')
-	search_replace(ADSWorker $newname './*.py')
-	search_replace(ADSWorker $newname './Vagrantfile')
+if [ "${answer:-y}" == "y" ]
+     then
+         search_replace 'ADSWorker' $newname './ADSWorker'
+         search_replace 'ADSWorker' $newname './alembic'
+         search_replace 'ADSWorker' $newname './manifests'
+         search_replace 'ADSWorker' $newname './*.ini'
+         search_replace 'ADSWorker' $newname './*.md'
+         search_replace 'ADSWorker' $newname './*.txt'
+         search_replace 'ADSWorker' $newname './*.py'
+         search_replace 'ADSWorker' $newname './Vagrantfile'
 	
-	mv ADSWorker $newname
+         mv ADSWorker $newname
 fi
 
+
+if ! hash virtualenv 2>/dev/null
+     then
+         read -p "You need virtualenv, can we install it for you via sudo?:" answer
+         if [ "${answer:-y}" == "y" ]
+             then 
+                  echo "sudo pip install virtualenv"
+                  sudo pip install virtualenv
+         fi
+     else
+          echo "Cannot continue if you do not install virtualenv, aborting"
+          exit 1  
+fi
+         
 virtualenv python
 source python/bin/activate
 
